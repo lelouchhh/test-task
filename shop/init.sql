5cc5240e627eb78bf79990e0ba81bc169f83ac65
Grigoriy <42106017+lelouchhh@users.noreply.github.com>
2023-07-28 10:40:44 +0700

    first

--- shop/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       first_name VARCHAR(100) NOT NULL,
-                       second_name VARCHAR(100),
-                       last_name VARCHAR(100) NOT NULL,
-                       email VARCHAR(255) NOT NULL UNIQUE,
-                       debt NUMERIC CHECK (debt >= 0 AND debt <= 1000) DEFAULT 0
-);
--- stock-service/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE algorithm (
-                           id SERIAL PRIMARY KEY,
-                           name TEXT NOT NULL,
-                           task TEXT NOT NULL,
-                           solution TEXT,
-                           price REAL
-);
